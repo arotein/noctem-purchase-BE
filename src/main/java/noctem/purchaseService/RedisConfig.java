6c2bfe6e5195ee734b9e3f534bc8545e81558d13
arotein <9cksqhdel3@naver.com>
2022-11-02 19:52:30 +0900

    chore: set env -> .yml of git

--- src/main/java/noctem/purchaseService/RedisConfig.java
+++ src/main/java/noctem/purchaseService/RedisConfig.java
@@ -1,9 +1,12 @@
 package noctem.purchaseService;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.context.scope.refresh.RefreshScopeRefreshedEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.context.event.EventListener;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -14,6 +17,7 @@ import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@RefreshScope
 @EnableRedisRepositories
 public class RedisConfig {
     @Value("${spring.redis.host}")
@@ -57,5 +61,9 @@ public class RedisConfig {
     @Bean
     public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
         return new GenericJackson2JsonRedisSerializer();
+    }
+
+    @EventListener(RefreshScopeRefreshedEvent.class)
+    public void onRefresh() {
     }
 }
